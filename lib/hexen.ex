e1b1c00e91967d3bedff39509a4e87a6b516613b
DanCouper <danielcouper81@googlemail.com>
2017-03-16 10:34:57 +0000

    initial commit

---
+++ lib/hexen.ex
@@ -0,0 +1,18 @@
+defmodule Hexen do
+  @moduledoc """
+  Documentation for Hexen.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hexen.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hexen_test.exs
@@ -0,0 +1,8 @@
+defmodule HexenTest do
+  use ExUnit.Case
+  doctest Hexen
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
